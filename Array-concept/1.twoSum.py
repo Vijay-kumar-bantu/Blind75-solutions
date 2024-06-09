d417140d4bb5464277e49389a29870f03882cbdf
Vijay <capvijju24@gmail.com>
2024-06-09 13:26:56 +0530

    added two sum problem solution

---
+++ Array-concept/1.twoSum.py
@@ -0,0 +1,28 @@
+"""
+Easy category
+leetcode link: https://leetcode.com/problems/two-sum/description/
+"""
+
+
+#Solutin
+
+#the function will return the array of indexes where the sum of values will be equal to the target
+def getTwoSum(nums:list[int], target:int)->list[int]:
+        #a hashmap where each difference of every index will be stored
+        differenceMap = dict()
+
+        for i in range(len(nums)):
+            #checking whether the difference is already existed in the map
+            if nums[i] in differenceMap.keys():
+                #if existed returns the answer
+                return [differenceMap[nums[i]],i]
+            
+            #if not existed adding the difference of index to the map
+            differenceMap[target - nums[i]] = i
+
+#Example
+nums = [2,7,11,15]
+target = 9
+
+#output should be [0,1] for above example
+print(getTwoSum(nums,target))
